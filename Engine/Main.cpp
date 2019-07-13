fcfb3668b022d8d631771d29e0686350175701dc
Silmaril\Assen <asi.markov@gmail.com>
2019-07-13 18:34:47 +0300

    Set up OpenGL with GLFW and GLAD.

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
-#include"CoreSystems/Math/Vector3.h"
-#include"CoreSystems/Math/Vector4.h"
-#include"CoreSystems/Math/Math.h"
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include"CoreSystems/Math//Vector4.h"
 
 int main()
 {
-	system("pause");
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		return -1;
+	}
+	
+	glViewport(0, 0, 800, 600);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		Vector4 color(1.0f, 0.0f, 0.0f, 1.0f);
+		glClearBufferfv(GL_COLOR, NULL, &color[0]);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
