bb3c8ac8f93a42ed5a61188394a18e07b188b648
Silmaril\Assen <asi.markov@gmail.com>
2019-08-07 16:36:22 +0300

    Added glBindBufferRange wrapper functions to the Buffer class.

--- Engine/Graphics/Buffer.cpp
+++ Engine/Graphics/Buffer.cpp
@@ -13,9 +13,29 @@ Buffer::Buffer(size_t bufferSize, const void * data) :size(bufferSize)
 }
 
 
-void Buffer::SetData(int offset, int size, const void * data)
+void Buffer::SetData(size_t offset, size_t size, const void * data)
 {
 	glNamedBufferSubData(bufferID, offset, size, data);
+}
+
+void Buffer::BindUniform(unsigned int index, size_t offset, size_t size)
+{
+	glBindBufferRange(GL_UNIFORM_BUFFER, index, bufferID, offset, size);
+}
+
+void Buffer::BindShaderStorage(unsigned int index, size_t offset, size_t size)
+{
+	glBindBufferRange(GL_SHADER_STORAGE_BUFFER, index, bufferID, offset, size);
+}
+
+void Buffer::BindTransformFeedback(unsigned int index, size_t offset, size_t size)
+{
+	glBindBufferRange(GL_TRANSFORM_FEEDBACK_BUFFER, index, bufferID, offset, size);
+}
+
+void Buffer::BindAtomicCounter(unsigned int index, size_t offset, size_t size)
+{
+	glBindBufferRange(GL_ATOMIC_COUNTER_BUFFER, index, bufferID, offset, size);
 }
 
 GLuint Buffer::GetBufferID() const
--- Engine/Graphics/Buffer.h
+++ Engine/Graphics/Buffer.h
@@ -11,7 +11,12 @@ public:
 	Buffer(size_t bufferSize);
 	Buffer(size_t bufferSize, const void* data);
 
-	void SetData(int offset, int size, const void* data);
+	void SetData(size_t offset, size_t size, const void* data);
+
+	void BindUniform(unsigned int index, size_t offset, size_t size);
+	void BindShaderStorage(unsigned int index, size_t offset, size_t size);
+	void BindTransformFeedback(unsigned int index, size_t offset, size_t size);
+	void BindAtomicCounter(unsigned int index, size_t offset, size_t size);
 
 	GLuint GetBufferID() const;
 	size_t GetSize() const;
